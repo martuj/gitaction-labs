f4cfad763141c1490f0c6c13967b939bc96b0218
martuj <89678981+martuj@users.noreply.github.com>
2026-01-11 16:37:18 +0530

    Update and rename demo to App.java

---
+++ App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Docker GitHub Actions!");
+        while (true) {
+            try {
+                Thread.sleep(10000);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
+
